88e37bf20acb1e8e73b4027719ce6c0786aa0b57
Naman Pandey <naman.0816@gmail.com>
2024-10-10 15:40:53 +0530

    second commit

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterVideoForm.tsx
@@ -0,0 +1,105 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+import * as z from "zod";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { Pencil, PlusCircle, VideoIcon } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+import { Chapter, MuxData } from "@prisma/client";
+import MuxPlayer from "@mux/mux-player-react";
+import { FileUpload } from "@/components/FileUpload";
+
+interface ChapterVideoFormProps {
+  initialData: Chapter & { muxData?: MuxData | null };
+  courseId: string;
+  chaptedId: string;
+}
+
+const formSchema = z.object({
+  videoUrl: z.string().min(1),
+});
+
+const ChapterVideoForm = ({
+  initialData,
+  courseId,
+  chaptedId,
+}: ChapterVideoFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const router = useRouter();
+  const toggleEdit = () => {
+    setIsEditing((current) => !current);
+  };
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapters/${chaptedId}`,
+        values
+      );
+      toast.success("Chapter Updated");
+      toggleEdit();
+      router.refresh();
+    } catch (error) {
+      toast.error("Something Went Wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Chapter Video
+        <Button variant="ghost" onClick={toggleEdit}>
+          {isEditing && <>Cancel</>}
+          {!isEditing && !initialData.videoUrl && (
+            <>
+              <PlusCircle className="size-4 mr-2" />
+              Add a video
+            </>
+          )}
+          {!isEditing && initialData.videoUrl && (
+            <>
+              <Pencil className="size-4 mr-2" />
+              Edit video
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing &&
+        (!initialData.videoUrl ? (
+          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+            <VideoIcon className="size-10 text-slate-500" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <MuxPlayer playbackId={initialData?.muxData?.playbackId || ""} />
+          </div>
+        ))}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="chapterVideo"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ videoUrl: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            Upload this chapter&apos;s video
+          </div>
+        </div>
+      )}
+      {initialData.videoUrl && !isEditing && (
+        <div className="text-xs text-muted-foreground mt-2">
+          Videos can take a few minutes to process. Refresh the page if video
+          does not appear.
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ChapterVideoForm;
